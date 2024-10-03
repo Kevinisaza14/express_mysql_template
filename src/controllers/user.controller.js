f0892fe6b8ecbaa988792c9cdb74581ebe3f34f1
kevinisaza14 <kevinisaza16285@gmail.com>
2024-10-03 18:07:47 +0200

    act

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-const dbConnection = require("../config/dbConnection.js");
+const db = require("../config/dbMySQL.js");
 const { v4: uuidv4 } = require('uuid');
-// uuidv4()
 // "G6A(7Hr|_ed$jr7#5k}?"
 const CryptoJS = require("crypto-js");
+
 const Joi = require("joi");
 // creamos schema de validacion de user
 const userSchema = Joi.object({
@@ -12,13 +12,62 @@ const userSchema = Joi.object({
 });
 
 exports.getAllUsers = async (req, res) => {
+    let connection; // declarada fuera para que se pueda acceder desde cada bloque
     try {
-        const [result] = await dbConnection.query("select * from users");
-        return res.status(200).json({result});
+        connection = await db.getConnection();
+        const [result] = await connection.query("select * from users");
+        // TODO: Verificar por que no sale el mensaje "No se encontraron usuarios"
+        if (result.length === 0) {
+            return res.status(204).json({
+                message: "No se encontraron usuarios"
+            });
+        }
+        return res.status(200).json({
+            message: "Lista de usuarios encontrados",
+            result
+        });
     } catch (error) {
-        return res.status(500).json({ 
+        return res.status(500).json({
             message: "No se pudo obtener los usuarios",
             error: "Error 500: " + error
         });
+    } finally {
+        connection.release();
     }
 }
+
+exports.createUser = async (req, res) => {
+    let connection;
+    try {
+        // validar 
+        const { error } = userSchema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                message: error.details[0].message,
+                error: "Error de validación"
+            });
+        }
+        // destructuring object
+        const { username, email, password } = req.body;
+        // TODO: Validar que el email no este registrado en MySQL
+        const id = uuidv4();
+        const hashPassword = CryptoJS.AES.encrypt(password, process.env.CRYPTO_SECRET).toString();
+        // evitar inyección sql
+        connection = await db.getConnection();
+        const sql = "insert into users values (?, ?, ?, ?, default, default)";
+        await connection.query(sql, [id, username, email, hashPassword]);
+        return res.status(201).json({
+            message: "Usuario registrado correctamente",
+            user: { id, username, email, hashPassword }
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "No se pudo registrar usuario",
+            error: "Error 500: " + error
+        });
+    } finally {
+        connection.release();
+    }
+}
+
+// TODO: getUserById, updateUser, deleteUser
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,7 @@ const userController = require("../controllers/user.controller.js");
 // rutas
 router.get("/users", userController.getAllUsers);
 // router.get("/user/:id", userController.getUserById);
-// router.post("/user", userController.createUser);
+router.post("/user", userController.createUser);
 // router.put("/user/:id", userController.updateUser);
 // router.delete("/user/:id", userController.deleteUser);
 
