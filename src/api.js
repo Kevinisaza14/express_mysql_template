f6e8ca60958e6fb22a00861bb6b7d7b5a86030b3
kevinisaza14 <kevinisaza16285@gmail.com>
2024-10-14 20:05:00 +0200

    Agregar documentación de la API con Swagger

--- src/api.js
+++ src/api.js
@@ -11,6 +11,32 @@ const apiRoutes = require("./routes/api.routes.js");
 api.use(express.json());
 api.use(cors());
 api.use("/api/v1",userRoutes, authRoutes, apiRoutes);
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "express_Mysql_template",
+            version: "1.0.0",
+            description: "Plantilla API REST con ExpressJS y MySQL",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+        },
+        servers: [
+            {
+              url: "http://localhost:3000/docs",
+            },
+          ],
+    },
+    apis: ["./routes/*.js"],
+}
+const swaggerSpec = swaggerJsdoc(options);
+api.use(
+    "/api-docs", 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerSpec, { explorer: true })
+);
 api.use((req, res) => { // 404
     res.status(404).json({ 
         message: "Ruta no encontrada",
